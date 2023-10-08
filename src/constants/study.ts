b8d9875b0650e8b2776eb460504e7c73e9958cb9
Brian M. Smith <brian@brianmatthewsmith.com>
2023-10-08 11:37:05 -0400

    More studyguide questions

--- src/constants/study.ts
+++ src/constants/study.ts
@@ -107,10 +107,137 @@ export const STUDY_GUIDE: Record<
       Integrating React into a traditional MVC framework requires some additional configuration.
       The code complexity increases with inline templating and JSX.
       Too many smaller components leading to over engineering or boilerplate.`
+    },
+    {
+      question: 'What is the useCallback hook?',
+      answer: `The primary purpose of useCallback is to memoize functions and prevent unnecessary re-creation of function instances when a component re-renders. It's useful when you need to pass functions as props to child components. By memoizing the function, you can prevent unnecessary re-renders of child components when the parent component re-renders.
+      It's commonly used with event handlers and functions that are part of the component's state or effect dependencies.`
+    },
+    {
+      question: 'What is the useMemo hook?',
+      answer: `The primary purpose of useMemo is to memoize values (not functions) and prevent the re-computation of expensive calculations or expensive object creations when a component re-renders. It's useful when you have a calculation that is computationally expensive, and you want to avoid recalculating it every time the component renders.
+      It's often used for memoizing the results of complex computations, formatting data, or creating new objects that depend on certain inputs.`
+    },
+    {
+      question: 'useCallback vs useMemo summary',
+      answer: `Use useCallback when you want to memoize functions to prevent unnecessary function re-creation and you're primarily concerned with optimizing function references.
+      Use useMemo when you want to memoize values or object instances to prevent unnecessary recomputation of expensive calculations or object creations.
+      Remember that both hooks should be used judiciously. Using them too aggressively can lead to over-optimization, which might make your code harder to understand. Use profiling tools and performance metrics to determine if and where these optimizations are necessary for your specific application.`
+    },
+    {
+      question: 'What is a compound component?',
+      answer: `A compound component in React is a pattern used to create a set of interconnected components that work together to form a complex UI component. Unlike traditional components, where each component manages its own state and rendering logic, compound components share state and behavior among themselves while allowing developers to configure their appearance and behavior individually.`
+    },
+    {
+      question: 'useEffect vs useLayoutEffect',
+      answer: `
+      useEffect and useLayoutEffect are both hooks in React used for handling side effects in functional components. However, they differ in when they are executed during the component's lifecycle and whether they block rendering.
+
+      useEffect:
+
+      Execution Timing: useEffect is called asynchronously after rendering is committed to the screen. This means it runs after the render cycle is complete, and the DOM has been updated.
+
+      Blocking Rendering: useEffect does not block the rendering process. It allows the component to render first, and then it performs its side effects. Therefore, any changes it makes to the DOM or state won't be reflected until the next render cycle.
+
+      Common Use Cases: Use useEffect for side effects that don't need to be synchronous with rendering. This is suitable for most scenarios, such as data fetching, setting up subscriptions, or performing DOM manipulations that don't impact layout.
+
+      useLayoutEffect:
+
+      Execution Timing: useLayoutEffect is called synchronously immediately after rendering but before the browser has painted the screen. This means it runs before the render cycle is complete and before any changes to the DOM are visible to the user.
+
+      Blocking Rendering: useLayoutEffect blocks the rendering process. If you perform complex or time-consuming tasks within it, it may lead to slower rendering and a less responsive user interface.
+
+      Common Use Cases: Use useLayoutEffect when you need to make changes that affect the layout of the DOM and need to be reflected immediately. For example, when you want to measure the size of an element, you'd use useLayoutEffect to ensure that you have access to accurate layout information before the browser repaints.
+      `
     }
   ],
   Redux: [],
   Javascript: [
+    {
+      question: 'What is event propagation?',
+      answer: `Event propagation in JavaScript refers to the process by which events triggered by user interactions, such as mouse clicks or keyboard presses, are propagated or "bubbled" through the DOM (Document Object Model) hierarchy from the target element that originally triggered the event up to the root of the document or down to a specific target element, depending on the phase of propagation. The event propagation process allows multiple elements in the DOM hierarchy to observe and respond to the same event, and it provides a way to delegate event handling to ancestor elements, which can be more efficient than attaching event handlers to individual elements.`
+    },
+    {
+      question: 'What is event capturing?',
+      answer: `
+      Event capturing is one of the two phases of event propagation in JavaScript, the other being event bubbling. Event capturing occurs before the event reaches the target element, and it involves the propagation of the event from the root of the DOM (Document Object Model) hierarchy down to the target element that triggered the event.
+
+      Here's how event capturing works:
+
+      Event Triggering: When a user interacts with a web page, such as clicking a button or hovering over an element, an event is triggered, such as a click event or a mouseover event.
+
+      Event Capturing Phase: During the event capturing phase, the event starts at the root of the DOM hierarchy, which is typically the document object, and then travels down through the hierarchy of ancestor elements toward the target element. It descends from the root element through its parent elements, then their parent elements, and so on.
+
+      Target Element: Once the event reaches the target element that originally triggered the event, it enters the target phase. This is where event handlers registered on the target element itself are executed.
+
+      Event Bubbling Phase: After the target phase, the event proceeds to the event bubbling phase, where it starts to bubble up through the hierarchy from the target element, passing through its parent elements, and continuing up to the root element (document). During this phase, event handlers on each element along the propagation path have an opportunity to respond to the event.
+      `
+    },
+    {
+      question: 'What is event bubbling?',
+      answer: `Event bubbling allows you to capture events at higher levels of the DOM hierarchy, making it easier to handle events for multiple elements with a common ancestor. It simplifies event delegation and can lead to more efficient event handling.
+
+      Event Triggering: When a user interacts with a web page, such as clicking a button or clicking on an element, an event is triggered, such as a click event.
+
+      Event Propagation: The event starts at the target element where it was triggered and then bubbles up through the hierarchy of ancestor elements. It travels from the target element through its parent elements, then their parent elements, and so on, all the way up to the root element, which is typically the document object.
+
+      Event Handling: During the bubbling phase, each element along the propagation path has the opportunity to respond to the event by executing event handlers registered on those elements. Event handlers can be functions that perform specific actions in response to the event, such as changing the content of an element, toggling a CSS class, or triggering other JavaScript functions.
+      `
+    },
+    {
+      question: 'Explain the concept of "hoisting" in JavaScript.',
+      answer: `Hoisting is a JavaScript behavior where variable and function declarations are moved to the top of their containing scope during compilation. This means that you can use variables and functions before they are declared, but they will be initialized with undefined until their declarations are encountered in the code.`
+    },
+    {
+      question:
+        'What are arrow functions in ES6, and how do they differ from regular functions?',
+      answer: `Arrow functions are a concise way to write functions in ES6. They have a shorter syntax and automatically capture the this value from the surrounding lexical context. Arrow functions do not have their own this, arguments, or super binding, making them particularly useful for functions within classes or callbacks.`
+    },
+    {
+      question: 'Function expression vs function declaration',
+      answer: `Key differences of arrow function expressions:
+
+      Arrow functions do not have their own this. They inherit the this value from their enclosing lexical scope.
+      They cannot be used as constructor functions with the new keyword.
+      Arrow functions are always anonymous; you cannot give them a name.
+      They are often preferred for short, simple functions and for functions that need to preserve the value of this.
+      In summary, function declarations are hoisted and named, function expressions are not hoisted and can be anonymous or named, and arrow function expressions have a distinct syntax and inherit this from their lexical scope. The choice between them depends on your specific use case and coding style preferences.`
+    },
+    {
+      question:
+        'Describe the concept of the "this" keyword in JavaScript and how it behaves in different contexts.',
+      answer: `The this keyword refers to the current execution context in JavaScript. Its value depends on how and where a function is called:
+
+      In a regular function, this is determined by the function's caller.
+      In an arrow function, this is lexically scoped and retains the value of this from its surrounding code.`
+    },
+    {
+      question:
+        'What is a generator function, and how does it differ from a regular function?',
+      answer: `A generator function is a special type of function in JavaScript that allows you to pause and resume its execution. It's defined using an asterisk (function*). When a generator function is called, it returns an iterator object that can be used to control the flow of execution using the yield keyword.`
+    },
+    {
+      question: 'hat is the purpose of the bind() method in JavaScript?',
+      answer: `The bind() method allows you to create a new function with a specified this value and initial arguments. It's commonly used to set the context (the value of this) for a function, especially in event handlers and callback functions.`
+    },
+    {
+      question:
+        'What is the difference between null and undefined in JavaScript?',
+      answer: `null represents an intentionally missing value or absence of an object value. undefined represents a variable that has been declared but has not been assigned a value, or a non-existent object property.`
+    },
+    {
+      question:
+        'Explain the differences between let, const, and var in terms of variable declaration in ES6.',
+      answer: `var has function scope and can be re-declared within the same function.
+      let and const have block scope and cannot be re-declared within the same block.
+      const is used for variables that should not be reassigned after declaration, while let allows reassignment.`
+    },
+    {
+      question:
+        'Explain Promises and the role they play in asynchronous JavaScript.',
+      answer:
+        'Promises are objects that represent the eventual completion or failure of an asynchronous operation. They provide a more structured way to handle asynchronous code compared to callbacks. Promises have three states: pending, resolved (fulfilled), and rejected. Developers can attach .then() and .catch() handlers to handle successful and failed outcomes of asynchronous tasks.'
+    },
     {
       question: 'What is a constructor function?',
       answer:
@@ -119,7 +246,7 @@ export const STUDY_GUIDE: Record<
     {
       question: 'What is a closure?',
       answer:
-        "A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time."
+        "A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time. Closures are important because they allow for data encapsulation, private variables, and the creation of functions with persistent state."
     },
     {
       question: 'What is a stale closure?',
